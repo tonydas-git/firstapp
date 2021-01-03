82c4788ef7429c229597dad33aa23c3b68be199b
tonydas-git <76574524+tonydas-git@users.noreply.github.com>
2021-01-02 18:57:16 -0800

    Add files via upload

--- database/pool.js
+++ database/pool.js
@@ -1,1 +1,15 @@
-d
+'use strict';
+
+const mysql = require('promise-mysql');
+
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER_NAME_LOCAL,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_LOCAL_NAME,
+  charset: 'utf8mb4',
+  debug: false
+});
+
+module.exports = pool;
